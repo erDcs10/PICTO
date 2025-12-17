0c3089d71ce9d820990fafaf5857ce55450aec11
Primaaaaa24 <afdamukh@gmail.com>
2025-12-18 02:08:14 +0700

    membuat Navbar berfungsi

--- app/src/main/java/id/ac/pnm/photofilterapp/MainActivity.kt
+++ app/src/main/java/id/ac/pnm/photofilterapp/MainActivity.kt
@@ -3,6 +3,8 @@ package id.ac.pnm.photofilterapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import id.ac.pnm.photofilterapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,8 +12,19 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
-        
+
         super.onCreate(savedInstanceState)
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(viewBinding.root) }
+        setContentView(viewBinding.root)
+
+        // Cari NavHostFragment menggunakan supportFragmentManager
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        
+        // Ambil NavController dari NavHostFragment
+        val navController = navHostFragment.navController
+
+        // Hubungkan bottomNavigationView dengan navController
+        viewBinding.bottomNavigationView.setupWithNavController(navController)
+    }
 }
